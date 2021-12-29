7bbd0e3080a7a18dc26b38e88c31e8e2e14b476a
latentfreedom <latentfreedom@gmail.com>
2021-12-28 23:53:18 -0500

    Implement use of Guild ID. Allow commands to only be used in a single guild given env value. Make style functions not async. chain continuous message embed calls together.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let data = {};
 
 client.on('ready', () => {
     console.log('Project Info Running...');
-    createCommands();
+    createCommandsForGuilds();
     getJsonData();
 });
 
@@ -20,39 +20,39 @@ const getJsonData = () => {
     console.log(data);
 }
 
-const styleHelpMessage = async () => {
-    const embed = new MessageEmbed().setTitle('Command Help');
-    embed.setDescription('The following are the available project info commands.')
-    embed.addFields(
-        { name: '/supply', value: 'Show the total supply for the mint' },
-        { name: '/price', value: 'Show the price for a single mint' },
-        { name: '/about', value: 'Show info about the project' },
-        { name: '/links', value: 'Send the official project links' },
-        { name: '/release', value: 'Show the release date for the project' },
-        { name: '/sneakpeek', value: 'Send a sneak peek picture for the project' },
-    );
+const styleHelpMessage = () => {
+    const embed = new MessageEmbed().setTitle('Command Help')
+        .setDescription('The following are the available project info commands.')
+        .addFields(
+            { name: '/supply', value: 'Show the total supply for the mint' },
+            { name: '/price', value: 'Show the price for a single mint' },
+            { name: '/about', value: 'Show info about the project' },
+            { name: '/links', value: 'Send the official project links' },
+            { name: '/release', value: 'Show the release date for the project' },
+            { name: '/sneakpeek', value: 'Send a sneak peek picture for the project' },
+        );
     return {embeds: [embed]};   
 }
 
-const styleSupplyMessage = async () => {
-    const embed = new MessageEmbed().setTitle('Project Supply');
-    embed.setDescription(`There will be a total of ${data.projectinfo.supply}`);
+const styleSupplyMessage = () => {
+    const embed = new MessageEmbed().setTitle('Project Supply')
+        .setDescription(`There will be a total of ${data.projectinfo.supply}`);
     return {embeds: [embed]}; 
 }
 
-const stylePriceMessage = async () => {
-    const embed = new MessageEmbed().setTitle('Mint Price');
-    embed.setDescription(`The price for a single mint is ${data.projectinfo.price}`);
+const stylePriceMessage = () => {
+    const embed = new MessageEmbed().setTitle('Mint Price')
+        .setDescription(`The price for a single mint is ${data.projectinfo.price}`);
     return {embeds: [embed]}; 
 }
 
-const styleAboutMessage = async () => {
-    const embed = new MessageEmbed().setTitle(`${data.projectinfo.name}`);
-    embed.setDescription(data.projectinfo.about)
+const styleAboutMessage = () => {
+    const embed = new MessageEmbed().setTitle(`${data.projectinfo.name}`)
+        .setDescription(data.projectinfo.about)
     return {embeds: [embed]};
 }
 
-const styleOfficialLinksMessage = async () => {
+const styleOfficialLinksMessage = () => {
     const embed = new MessageEmbed().setTitle('Official Links');
     for (let [title, link] of Object.entries(data.projectinfo.links)) {
         embed.addFields({ name: title, value: link});
@@ -60,65 +60,73 @@ const styleOfficialLinksMessage = async () => {
     return {embeds: [embed]}; 
 }
 
-const styleReleaseMessage = async () => {
-    const embed = new MessageEmbed().setTitle('Release Date');
-    embed.setDescription(`The project will be released on ${data.projectinfo.release}`);
+const styleReleaseMessage = () => {
+    const embed = new MessageEmbed().setTitle('Release Date')
+        .setDescription(`The project will be released on ${data.projectinfo.release}`);
     return {embeds: [embed]};
 }
 
-const styleSneakPeekMessage = async () => {
+const styleSneakPeekMessage = () => {
     const files = fs.readdirSync(data.projectinfo.sneakpeek);
     let chosenFile = files[Math.floor(Math.random() * files.length)];
     const path = `${data.projectinfo.sneakpeek}/${chosenFile}`;
     const embed = new MessageEmbed().setTitle('Sneak Peek')
         .setDescription(`Check out this sneak peek from the vault!`)
         .setImage(`attachment://${path}`);
-    return {embeds: [embed], files: [`${path}`]}; 
+    return {embeds: [embed], files: [path]}; 
 }
 
-
-const createCommands = () => {
-    const Guilds = client.guilds.cache.map(guild => guild.id);
-    // Add commands to all guilds
-    Guilds.forEach(guildId => {
-        const guild = client.guilds.cache.get(guildId);
-        let commands = guild.commands;
-        // Show Help
-        commands?.create({
-            name: "help",
-            description: "Show all the commands possible"
-        });
-        // Show Supply
-        commands?.create({
-            name: "supply",
-            description: "Show the total supply for the mint"
-        });
-        // Show Price
-        commands?.create({
-            name: "price",
-            description: "Show the price for a single mint"
-        });
-        // Show About
-        commands?.create({
-            name: "about",
-            description: "Show info about the project"
-        });
-        // Send Official Links
-        commands?.create({
-            name: "links",
-            description: "Send the official project links"
-        });
-        // Show Release Date
-        commands?.create({
-            name: "release",
-            description: "Show the release date for the project"
-        });
-        // Show Sneak Peek
-        commands?.create({
-            name: "sneakpeek",
-            description: "Send a sneak peek picture for the project"
-        });
+const createCommands = (guildId) => {
+    const guild = client.guilds.cache.get(guildId);
+    let commands = guild.commands;
+    // Show Help
+    commands?.create({
+        name: "help",
+        description: "Show all the commands possible"
     });
+    // Show Supply
+    commands?.create({
+        name: "supply",
+        description: "Show the total supply for the mint"
+    });
+    // Show Price
+    commands?.create({
+        name: "price",
+        description: "Show the price for a single mint"
+    });
+    // Show About
+    commands?.create({
+        name: "about",
+        description: "Show info about the project"
+    });
+    // Send Official Links
+    commands?.create({
+        name: "links",
+        description: "Send the official project links"
+    });
+    // Show Release Date
+    commands?.create({
+        name: "release",
+        description: "Show the release date for the project"
+    });
+    // Show Sneak Peek
+    commands?.create({
+        name: "sneakpeek",
+        description: "Send a sneak peek picture for the project"
+    });
+}
+
+const createCommandsForGuilds = () => {
+    // Check if guild id set
+    if (process.env.GUILD_ID) {
+        createCommands(process.env.GUILD_ID);
+    } else {
+        const Guilds = client.guilds.cache.map(guild => guild.id);
+        // Add commands to all guilds
+        Guilds.forEach(guildId => {
+            createCommands(guildId);
+        });
+    }
 }
 
 client.on('interactionCreate', async (interaction) => {
@@ -136,19 +144,19 @@ client.on('interactionCreate', async (interaction) => {
         let content = {};
         await interaction.deferReply({});
         if (commandName === 'help') {
-            content = await styleHelpMessage();
+            content = styleHelpMessage();
         } else if (commandName === 'supply') {
-            content = await styleSupplyMessage();
+            content = styleSupplyMessage();
         } else if (commandName === 'price') {
-            content = await stylePriceMessage();
+            content = stylePriceMessage();
         } else if (commandName === 'about') {
-            content = await styleAboutMessage();
+            content = styleAboutMessage();
         } else if (commandName === 'links') {
-            content = await styleOfficialLinksMessage();
+            content = styleOfficialLinksMessage();
         } else if (commandName === 'release') {
-            content = await styleReleaseMessage();
+            content = styleReleaseMessage();
         } else if (commandName === 'sneakpeek') {
-            content = await styleSneakPeekMessage();
+            content = styleSneakPeekMessage();
         }
         await interaction.editReply(content);
     } catch (err) {
